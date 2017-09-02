bd1b60ba4f44ce15f794d4813ca375f98c64001d
陈式超 <1948404397@qq.com>
2017-09-02 15:39:43 +0800

    设置界面

---
+++ Healths/Healths/Controller/SRViewController.h
@@ -0,0 +1,13 @@
+//
+//  SRViewController.h
+//  Healths
+//
+//  Created by admin on 2017/9/2.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SRViewController : UIViewController
+
+@end
---
+++ Healths/Healths/Controller/XBViewController.h
@@ -0,0 +1,13 @@
+//
+//  XBViewController.h
+//  Healths
+//
+//  Created by admin on 2017/9/2.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XBViewController : UIViewController
+
+@end
