42dd007620c509808ca98f675e062a3d28646050
雷贤焕 <1437918286@qq.com>
2017-09-02 17:19:47 +0800

    我的

---
+++ Healths/Healths/View/MyTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  MyTableViewCell.h
+//  Healths
+//
+//  Created by 233 on 2017/9/2.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *MyIdenLabel;
+
+@end
