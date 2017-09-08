14f3c930cfa7a87a5042cf4fcd6fc5bdf8b200f1
陈永剑 <1776968161@qq.com>
2017-09-08 14:48:40 +0800

    体验卡

--- Healths/Healths/Controller/ClubDetailViewController.m
+++ Healths/Healths/Controller/ClubDetailViewController.m
@@ -57,6 +57,22 @@
 // 这个方法专门做导航条的控制
 -(void)naviConfig{
     //设置导航条标题文字
+    self.navigationItem.title=@"体验卡信息";
+    //设置导航条的颜色（风格颜色）
+    [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithRed:20/255.0 green:100/255.0 blue:255.0 alpha:1.0]];
+    //设置导航条的标题颜色
+    self.navigationController.navigationBar.titleTextAttributes=@{NSForegroundColorAttributeName : [UIColor whiteColor] };
+    //设置导航条是否隐藏
+    self.navigationController.navigationBar.hidden=NO;
+    //设置导航条上按钮的风格颜色
+    self.navigationController.navigationBar.tintColor=[UIColor whiteColor];
+    //设置是否需要毛玻璃效果
+    self.navigationController.navigationBar.translucent=YES;
+}
+
+// 这个方法专门做导航条的控制
+-(void)naviConfiga{
+    //设置导航条标题文字
     self.navigationItem.title=@"会所信息";
     //设置导航条的颜色（风格颜色）
     self.navigationController.navigationBar.barTintColor=[UIColor brownColor];
