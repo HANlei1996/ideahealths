2d16f34576a309d7b921298c7890144dfc8b3983
雷贤焕 <1437918286@qq.com>
2017-09-13 18:21:52 +0800

    我的推广

--- Healths/Healths/Controller/MyPromotionViewController.m
+++ Healths/Healths/Controller/MyPromotionViewController.m
@@ -12,6 +12,7 @@
 @interface MyPromotionViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *erweimaImage;
 @property (strong, nonatomic) UIActivityIndicatorView *avi;
+@property (strong,nonatomic)NSString *er;
 @end
 
 @implementation MyPromotionViewController
@@ -26,23 +27,54 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    // 1.创建过滤器 -- 苹果没有将这个字符封装成常量
-    CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+-(void)requst{
+    _avi = [Utilities getCoverOnView:self.view];
     
-    // 2.过滤器恢复默认设置
-    [filter setDefaults];
+    [RequestAPI requestURL:@"/mySelfController/getInvitationCode" withParameters:@{@"memberId":@2} andHeader:nil byMethod:kGet andSerializer:kForm success:^(id responseObject) {
+        [_avi stopAnimating];
+        NSLog(@"mypromotion:%@", responseObject);
+        if ([responseObject[@"resultFlag"]integerValue]==8001) {
+            _er = responseObject[@"result"];
+            // 1.创建过滤器 -- 苹果没有将这个字符封装成常量
+            CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+            
+            // 2.过滤器恢复默认设置
+            [filter setDefaults];
+            
+            // 3.给过滤器添加数据(正则表达式/帐号和密码) -- 通过KVC设置过滤器,只能设置NSData类型
+            NSString *dataString = [NSString stringWithFormat:@"http://dwz.cn/%@",_er];
+            NSData *data = [dataString dataUsingEncoding:NSUTF8StringEncoding];
+            [filter setValue:data forKeyPath:@"inputMessage"];
+            
+            // 4.获取输出的二维码
+            CIImage *outputImage = filter.outputImage;
+            UIImage *image = [self createNonInterpolatedUIImageFormCIImage:outputImage withSize:200];
+            // 5.显示二维码
+            _erweimaImage.image = image;
+
+        }
+        else{
+            [_avi stopAnimating];
+            NSString *errorMsg=[ErrorHandler getProperErrorString:[responseObject[@"resultFlag"]integerValue]];
+            [Utilities popUpAlertViewWithMsg:errorMsg andTitle:nil onView:self];
+        }
+    } failure:^(NSInteger statusCode, NSError *error) {
+        [_avi stopAnimating];
+        [Utilities popUpAlertViewWithMsg:@"网络错误,请稍等再试" andTitle:@"提示" onView:self];
+    }];
     
-    // 3.给过滤器添加数据(正则表达式/帐号和密码) -- 通过KVC设置过滤器,只能设置NSData类型
-    NSString *dataString = @"ETumE";
-    NSData *data = [dataString dataUsingEncoding:NSUTF8StringEncoding];
-    [filter setValue:data forKeyPath:@"inputMessage"];
     
-    // 4.获取输出的二维码
-    CIImage *outputImage = [filter outputImage];
-    
-    // 5.显示二维码
-    self.erweimaImage.image = [UIImage imageWithCIImage:outputImage];
 }
 - (UIImage *)createNonInterpolatedUIImageFormCIImage:(CIImage *)image withSize:(CGFloat) size
 {
@@ -67,38 +99,5 @@
     return [UIImage imageWithCGImage:scaledImage];
 }
 
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
--(void)requst{
-    _avi = [Utilities getCoverOnView:self.view];
-    
-    [RequestAPI requestURL:@"/mySelfController/getInvitationCode" withParameters:@{@"memberId":@2} andHeader:nil byMethod:kGet andSerializer:kForm success:^(id responseObject) {
-        [_avi stopAnimating];
-        NSLog(@"mypromotion:%@", responseObject);
-        if ([responseObject[@"resultFlag"]integerValue]==8001) {
-            NSDictionary *result = responseObject[@"ETumE"];
-            NSString *list = result[@"result"];
-            [[StorageMgr singletonStorageMgr] removeObjectForKey:@"result"];
-            [[StorageMgr singletonStorageMgr] addKey:@"result" andValue:list];        }
-        else{
-            [_avi stopAnimating];
-            NSString *errorMsg=[ErrorHandler getProperErrorString:[responseObject[@"resultFlag"]integerValue]];
-            [Utilities popUpAlertViewWithMsg:errorMsg andTitle:nil onView:self];
-        }
-    } failure:^(NSInteger statusCode, NSError *error) {
-        [_avi stopAnimating];
-        [Utilities popUpAlertViewWithMsg:@"网络错误,请稍等再试" andTitle:@"提示" onView:self];
-    }];
-    
-    
-}
 
 @end
